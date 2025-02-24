edc70b5b5c64a693cba22af24e784f0e1cc70682
rakesh-1597 <bhatrakesh930@gmail.com>
2025-02-24 15:40:18 +0100

    using Float32 in controller interface and removed unnecessary code

--- src/clamp_controller/controller_interface/clamp_command_interface.py
+++ src/clamp_controller/controller_interface/clamp_command_interface.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Float32MultiArray, Bool
+from std_msgs.msg import Float32, Bool
 
 class ClampCmdInterface:
     def __init__(self):
         rospy.init_node("clamp_command_interface")
-        self.relativeRotationPub_ = rospy.Publisher("flexrowick/clamp_relative_rotation", Float32MultiArray, queue_size = 10)
-        self.absoluteRotationPub_ = rospy.Publisher("flexrowick/clamp_absolute_rotation", Float32MultiArray, queue_size= 10)
+        self.relativeRotationPub_ = rospy.Publisher("flexrowick/clamp_relative_rotation", Float32, queue_size = 10)
+        self.absoluteRotationPub_ = rospy.Publisher("flexrowick/clamp_absolute_rotation", Float32, queue_size= 10)
         self.calibrationCommandPub_ = rospy.Publisher("flexrowick/calibration_done_cmd", Bool, queue_size= 10)
         rospy.sleep(1)  #give some time for rosNode to register the publishers for 1 second
 
@@ -16,7 +16,7 @@ class ClampCmdInterface:
         except ValueError as e:
             rospy.loginfo(f"Error! Entered value is not of type Float:{e}")
         try:
-            self.relativeRotationPub_.publish(Float32MultiArray(data=[float(relative_rotation)]))
+            self.relativeRotationPub_.publish(Float32(data=float(relative_rotation)))
         except rospy.ROSException:
             rospy.loginfo("Failed to publish relative rotation value!")
 
@@ -26,7 +26,7 @@ class ClampCmdInterface:
         except ValueError as e:
             rospy.loginfo(f"Error! Entered value is not of type Float:{e}")
         try:
-            self.absoluteRotationPub_.publish(Float32MultiArray(data=[float(absolute_rotation)]))
+            self.absoluteRotationPub_.publish(Float32(data=float(absolute_rotation)))
         except rospy.ROSException:
             rospy.loginfo("Failed to publish absolute rotation value!")
     
--- src/clamp_controller/main.py
+++ src/clamp_controller/main.py
@@ -1,28 +1,10 @@
 #!/usr/bin/env python3
-
-import rospy
-import threading
-import os
-from std_msgs.msg import Float32MultiArray, Bool
-from plc_client_node.plcClientNode import PlcClient
 from controller_interface.clamp_command_interface import ClampCmdInterface
-import yaml
-
-def relRot(angle):
-    try:
-        relative_rotation = angle
-    except ValueError as e:
-        print(f"Error! Entered value is not of type Float:{e}")
-    try:
-        tpub_relative_rotation.publish(Float32MultiArray(data=[float(relative_rotation)]))
-    except rospy.ROSException:
-        print("Failed to publish relative rotation value!")
 
 def userInputLoop(interfaceObj:ClampCmdInterface):
     while True:
         print("Welcome to the FlexRoWick Clamp Controller terminal!")
         print("Command Menu: 1) Perform relative rotation 2) Perform absolute rotation 3) Perform calibration")
-        print("Enter the command number:")
         try:
             command = int(input("Enter the command number: "))
         except ValueError:
@@ -48,27 +30,6 @@ def userInputLoop(interfaceObj:ClampCmdInterface):
             interfaceObj.setCalibration(bool(flag))
         else:
             print("Invalid command! Please enter a valid command number.")
-def loadYaml():
-    """Loads parameters from a YAML file into the ROS Parameter Server"""
-    cwd = os.getcwd()
-
-    # Define the relative path to the YAML file (relative to the workspace root)
-    yaml_relative_path = "config/clamp_controller.yaml"
-    
-    # Construct the absolute path
-    yaml_path = os.path.join(cwd, yaml_relative_path)        
-    if not os.path.exists(yaml_path):
-        rospy.logwarn(f"YAML file not found: {yaml_path}")
-        return
-    
-    try:
-        with open(yaml_path, "r") as file:
-            params = yaml.safe_load(file)
-            for key, value in params.items():
-                rospy.set_param(key, value)  # Load params into ROS parameter server
-        print("Loaded parameters from YAML file.")
-    except Exception as e:
-        rospy.logerr(f"Failed to load YAML file: {e}")
 
 if __name__ == "__main__":
     clampCmdIf = ClampCmdInterface()
